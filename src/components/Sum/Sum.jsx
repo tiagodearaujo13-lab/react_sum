88be34582e1c2675de4b8134a7d0d0c4b475ff5b
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-25 08:01:13 +0000

    feat: implement Sum component with default props and string interpolation

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,3 +1,5 @@
-export const Sum = ({ a = 0, b = 0 }) => {
-  return `Sum of ${a} and ${b} is ${a + b}`;
-};
+export const Sum = ({ a = 0, b = 0 }) => (
+  <div>
+    <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+  </div>
+);
